c88b748f955a5ac092015146d2afa718bc9267c5
Mairbek Khadikov <mkhadikov@gmail.com>
2025-01-05 00:37:26 -0800

    color class

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <color.h>
 #include <vec3.h>
 
 int main() {
@@ -16,12 +17,8 @@ int main() {
       auto r = double(i) / (image_width - 1);
       auto g = double(j) / (image_height - 1);
       auto b = 0.0;
-
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
-
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+      auto pix_color = color(r, g, b);
+      write_color(std::cout, pix_color);
     }
   }
 }
